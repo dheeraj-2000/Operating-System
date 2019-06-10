74dd097a844851e28830bdfbed3f9e573e338468
Dheeraj Chaudhary <38909585+dheeraj-2000@users.noreply.github.com>
2019-06-10 23:31:10 +0530

    Implementation of simple shell executing few command

---
+++ command.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+int pipe1[2];
+int pipe2[2];
+
+void exec1()
+{
+  dup2(pipe1[1], 1);
+  close(pipe1[0]);
+  close(pipe1[1]);
+  execlp("cat", "cat","cmd.c", NULL);
+  //  execlp("ls", "ls", NULL);
+}
+
+void exec2()
+{
+  dup2(pipe1[0], 0);
+  dup2(pipe2[1], 1);
+  close(pipe1[0]);
+  close(pipe1[1]);
+  close(pipe2[0]);
+  close(pipe2[1]);
+  execlp("grep", "grep", "include", NULL);
+    //execlp("grep", "grep","e", NULL);
+}
+
+void exec3()
+{
+  dup2(pipe2[0], 0);
+  close(pipe2[0]);
+  close(pipe2[1]);
+  execlp("grep", "grep", "std",  NULL);
+
+}
+
+void main() {
+  pipe(pipe1);
+  if (fork() == 0)
+  {
+    exec1();
+  }
+  pipe(pipe2);
+  if (fork() == 0)
+  {
+    exec2();
+  }
+  close(pipe1[0]);
+  close(pipe1[1]);
+
+  if (fork() == 0)
+  {
+    exec3();
+  }
+}
